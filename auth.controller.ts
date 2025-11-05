f5184b533f2f12d2aa0748460b24e55a72d407a1
yyiannnnnn <yiannfong@gmail.com>
2025-11-05 21:28:14 +0800

    auth.controller.ts
    API endpoints for login

---
+++ auth.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Post, Request, UseGuards, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Web3AuthGuard } from './guards/web3-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(Web3AuthGuard)
+  @Post('web3-login')
+  async web3Login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
